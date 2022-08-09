75d980ac2a7ff3111dfff93cd4d2a3039bf5ec25
KadsonLima <kadson-kaio@hotmail.com>
2022-08-09 13:02:43 -0300

    feat: add Publish

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UsersView from "./Users/UsersView";
 import "../assets/reset.css";
 import "../assets/style.css";
+import Timeline from "./Timeline/Timeline";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/users/:1" element={ <UsersView /> } />
+        <Route path="/timeline" element={ <Timeline /> } />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Timeline/Timeline.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import Publish from "./Publish";
+
+function Timeline() {
+  return (
+    <Container>
+      <Content>
+        <h4>timeline</h4>
+        <Publish />
+      </Content>
+    </Container>
+  );
+}
+
+export default Timeline;
+
+
+
+const Container = styled.div`
+  padding-top: 78px;
+  margin-top: 72px;
+  display: flex;
+  width: 100%;
+  background-color: #333333;
+  height: calc(100vh - 72px);
+  align-items: center;
+  flex-direction: column;
+
+  
+`;
+
+const Content = styled.div`
+
+    h4 {
+        margin-bottom: 40px;
+        font-family: "Oswald";
+        font-weight: 700;
+        font-size: 43px;
+        color: white;
+    }
+`;
