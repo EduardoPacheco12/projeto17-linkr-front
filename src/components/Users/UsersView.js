a7ab1682f3f11ed6b3810ae1f69ff0758f0bf5e4
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-08-09 10:35:32 -0300

    feat: adjust screen size for desktop

--- src/components/Users/UsersView.js
+++ src/components/Users/UsersView.js
@@ -5,14 +5,16 @@ import Trends from "./Trends";
 function UsersView() {
   return (
     <MainContainer>
-      <UserDataContainer>
-        <img src="https://cdn.pixabay.com/photo/2017/01/01/22/04/crawl-1945633_960_720.jpg" alt="foca" />
-        <h2>{ `${"almost cleiton"}'s posts` }</h2>
-      </UserDataContainer>
-      <PostTrendContainer>
-        <Posts />
-        <Trends />
-      </PostTrendContainer>
+      <MainContent>
+        <UserDataContainer>
+          <img src="https://cdn.pixabay.com/photo/2017/01/01/22/04/crawl-1945633_960_720.jpg" alt="foca" />
+          <h2>{ `${"almost cleiton"}'s posts` }</h2>
+        </UserDataContainer>
+        <PostTrendContainer>
+          <Posts />
+          <Trends />
+        </PostTrendContainer>
+      </MainContent>
     </MainContainer>
   )
 }
@@ -20,14 +22,20 @@ function UsersView() {
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
   width: 100vw;
   min-height: 100vh;
-  padding: 0 240px;
   font-family: 'Lato', sans-serif;
   color: #FFFFFF;
   background-color: #333333;
   box-sizing: border-box;
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 900px;
 `;
 
 const UserDataContainer = styled.div`
