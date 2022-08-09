92529c243d1bfffb2a532687e1ada98caaaa7d87
Eduardo Pacheco <eduardooliveirapacheco131@gmail.com>
2022-08-08 21:48:35 -0300

    feat: add project inicial structure

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "../assets/css/reset.css";
+import "../assets/css/style.css";
+
+export default function App() {
+    
+}
